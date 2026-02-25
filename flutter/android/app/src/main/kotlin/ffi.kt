fe2582e5797e96068e826c5378242d6efc9c7548
郭建超 <cimoing@gmail.com>
2026-02-25 14:51:20 +0800

    Refactor package name from 'com.carriez.flutter_hbb' to 'com.imoing.flutter_hbb' across multiple files and update dependencies in pubspec.lock to newer versions with updated URLs.

--- flutter/android/app/src/main/kotlin/ffi.kt
+++ flutter/android/app/src/main/kotlin/ffi.kt
@@ -5,7 +5,7 @@ package ffi
 import android.content.Context
 import java.nio.ByteBuffer
 
-import com.carriez.flutter_hbb.RdClipboardManager
+import com.imoing.flutter_hbb.RdClipboardManager
 
 object FFI {
     init {
